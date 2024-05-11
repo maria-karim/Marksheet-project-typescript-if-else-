80bc8e70846ab86df394dd08a365369cdb9f9749
maria-karim <167464098+maria-karim@users.noreply.github.com>
2024-05-11 19:01:12 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,19 @@
+
+let Grade      : string = "C"
+let Percentage : number = 53
+
+
+if ( Grade == "A+" || Percentage == 80){
+    console.log("A+ congratulations")
+}
+ else if (Grade == "A" && Percentage >= 70) {console.log( "A-grade" )}
+
+ else if (Grade == "B" && Percentage >= 60) {console.log( "B-grade" )}
+
+ 
+ else if (Grade == "C" && Percentage >= 50) {console.log( "C-grade" )}
+
+ 
+ else if (Grade == "D" && Percentage >= 45) {console.log( "D-grade" )}
+
+ else  {console.log( "Failed" )}
